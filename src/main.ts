81735cecbc7f4db3ce49d218c7dc26f7fdae6ec4
volxdya <recommendation082@gmail.com>
2024-10-28 12:28:55 +0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './modules/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000, () => {
+    console.log(`http://localhost:3000`);
+  });
+
+
+}
+bootstrap();
